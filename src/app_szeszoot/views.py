d596577544f0208d6f6f70e835de25e312e4473a
AndrzejSzeszko <andrzejgustaw@gmail.com>
2019-01-04 18:52:05 +0100

    dummy stuff from django channels docs tutorial

--- src/app_szeszoot/views.py
+++ src/app_szeszoot/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
+import json
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'app_szeszoot/index.html', {})
+
+
+def room(request, room_name):
+    return render(request, 'app_szeszoot/room.html', {
+        'room_name_json': mark_safe(json.dumps(room_name)),
+    })
