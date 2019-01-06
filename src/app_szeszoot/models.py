4e0a1fa5853ca281ca8f1b565f4c7ab08c2f7e55
AndrzejSzeszko <andrzejgustaw@gmail.com>
2019-01-06 23:36:40 +0100

    add CustomUser model, add description field in Quiz model

--- src/app_szeszoot/models.py
+++ src/app_szeszoot/models.py
@@ -1,28 +1,37 @@
 from django.db import models
-from django.contrib.auth import settings
+from django.contrib.auth import (
+    settings,
+    models as auth_models
+)
+
+
+class CustomUser(auth_models.AbstractUser):
+    email = models.EmailField(unique=True)
 
 
 class Quiz(models.Model):
-    title = models.CharField(max_length=32)
+    title       = models.CharField(max_length=32, unique=True)
+    description = models.CharField(max_length=256)
 
 
 class Question(models.Model):
     question_content = models.CharField(max_length=256)
-    image = models.ImageField(upload_to='questions_images', null=True, blank=True)
-    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    image            = models.ImageField(upload_to='questions_images', null=True, blank=True)
+    quiz             = models.ForeignKey(Quiz, on_delete=models.CASCADE)
 
 
 class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    is_right = models.BooleanField(default=False)
-    player = models.ManyToManyField('Player')
+    question       = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_content = models.CharField(max_length=256)
+    is_correct     = models.BooleanField(default=False)
+    player         = models.ManyToManyField('Player')
 
 
 class Game(models.Model):
-    PIN = models.PositiveIntegerField()
+    PIN = models.PositiveIntegerField(null=True)
 
 
 class Player(models.Model):
-    nickname = models.CharField(max_length=32)
-    game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    nickname = models.CharField(max_length=32, unique=True)
+    game     = models.ForeignKey(Game, on_delete=models.CASCADE)
+    user     = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
