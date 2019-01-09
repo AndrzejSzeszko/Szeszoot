b3e9bf76ed16515ea619e52f278b3a02b7ed1ca7
AndrzejSzeszko <andrzejgustaw@gmail.com>
2019-01-09 23:38:29 +0100

    add path for player_panel

--- src/app_szeszoot/routing.py
+++ src/app_szeszoot/routing.py
@@ -5,5 +5,6 @@ from . import consumers
 
 websocket_urlpatterns = [
     path('ws/chat/<str:room_name>/', consumers.ChatConsumer),
-    path('ws/game_master_panel/<int:quiz_id>/<int:game_id>/', consumers.MasterGamePanelConsumer),
+    path('ws/game_master_panel/<int:quiz_pk>/<int:game_pk>/', consumers.MasterGamePanelConsumer),
+    path('ws/player_panel/<int:game_pk>/', consumers.PlayerPanelConsumer),
 ]
