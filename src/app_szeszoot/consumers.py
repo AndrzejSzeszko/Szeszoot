85e1b51f264014325ab0144f444baf8186ade8fa
AndrzejSzeszko <andrzejgustaw@gmail.com>
2019-01-10 23:48:23 +0100

    comment out PlayerPanelConsumer and ChatConsumer

--- src/app_szeszoot/consumers.py
+++ src/app_szeszoot/consumers.py
@@ -6,48 +6,48 @@ from .models import Player
 import json
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = f'chat_{self.room_name}'
-
-        # Join room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
-
-        await self.accept()
-
-    async def disconnect(self, close_code):
-        # Leave from group
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
-
-    # Receive message from WebSocket
-    async def receive(self, text_data=None, bytes_data=None):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        # Send message to the room group
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
-
-    # Receive message from room group
-    async def chat_message(self, event):
-        message = event['message']
-
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+# class ChatConsumer(AsyncWebsocketConsumer):
+#     async def connect(self):
+#         self.room_name = self.scope['url_route']['kwargs']['room_name']
+#         self.room_group_name = f'chat_{self.room_name}'
+#
+#         # Join room group
+#         await self.channel_layer.group_add(
+#             self.room_group_name,
+#             self.channel_name
+#         )
+#
+#         await self.accept()
+#
+#     async def disconnect(self, close_code):
+#         # Leave from group
+#         await self.channel_layer.group_discard(
+#             self.room_group_name,
+#             self.channel_name
+#         )
+#
+#     # Receive message from WebSocket
+#     async def receive(self, text_data=None, bytes_data=None):
+#         text_data_json = json.loads(text_data)
+#         message = text_data_json['message']
+#
+#         # Send message to the room group
+#         await self.channel_layer.group_send(
+#             self.room_group_name,
+#             {
+#                 'type': 'chat_message',
+#                 'message': message
+#             }
+#         )
+#
+#     # Receive message from room group
+#     async def chat_message(self, event):
+#         message = event['message']
+#
+#         # Send message to WebSocket
+#         await self.send(text_data=json.dumps({
+#             'message': message
+#         }))
 
 
 class MasterGamePanelConsumer(WebsocketConsumer):
@@ -96,44 +96,44 @@ class MasterGamePanelConsumer(WebsocketConsumer):
         }))
 
 
-class PlayerPanelConsumer(WebsocketConsumer):
-    def connect(self):
-        self.game_pk = self.scope['url_route']['kwargs']['game_pk']
-        self.player_group_name = f'player_game_{self.game_pk}'
-
-        # Join game group
-        async_to_sync(self.channel_layer.group_add)(
-            self.player_group_name,
-            self.channel_name
-        )
-
-    def disconnect(self, close_code):
-
-        # Leave game group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.player_group_name,
-            self.channel_name
-        )
-
-    # Receive data from WebSocket
-    def receive(self, text_data=None, bytes_data=None):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        # Send message to game group
-        async_to_sync(self.channel_layer.group_send)(
-            self.player_group_name,
-            {
-                'type': 'next_question_message',
-                'message': message
-            }
-        )
-
-    # Receive message from game group
-    def next_question_message(self, event):
-        message = event['message']
-
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+# class PlayerPanelConsumer(WebsocketConsumer):
+#     def connect(self):
+#         self.game_pk = self.scope['url_route']['kwargs']['game_pk']
+#         self.player_group_name = f'player_game_{self.game_pk}'
+#
+#         # Join game group
+#         async_to_sync(self.channel_layer.group_add)(
+#             self.player_group_name,
+#             self.channel_name
+#         )
+#
+#     def disconnect(self, close_code):
+#
+#         # Leave game group
+#         async_to_sync(self.channel_layer.group_discard)(
+#             self.player_group_name,
+#             self.channel_name
+#         )
+#
+#     # Receive data from WebSocket
+#     def receive(self, text_data=None, bytes_data=None):
+#         text_data_json = json.loads(text_data)
+#         message = text_data_json['message']
+#
+#         # Send message to game group
+#         async_to_sync(self.channel_layer.group_send)(
+#             self.player_group_name,
+#             {
+#                 'type': 'next_question_message',
+#                 'message': message
+#             }
+#         )
+#
+#     # Receive message from game group
+#     def next_question_message(self, event):
+#         message = event['message']
+#
+#         # Send message to WebSocket
+#         self.send(text_data=json.dumps({
+#             'message': message
+#         }))
