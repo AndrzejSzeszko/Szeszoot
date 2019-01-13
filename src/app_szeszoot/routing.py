b6dd969e66590a9188a00df0e0e1fd5825c94eed
AndrzejSzeszko <andrzejgustaw@gmail.com>
2019-01-13 11:25:41 +0100

    remove unused paths

--- src/app_szeszoot/routing.py
+++ src/app_szeszoot/routing.py
@@ -4,8 +4,5 @@ from . import consumers
 
 
 websocket_urlpatterns = [
-    # path('ws/chat/<str:room_name>/', consumers.ChatConsumer),
-    # path('ws/game_master_panel/<int:quiz_pk>/<int:game_pk>/', consumers.MasterGamePanelConsumer),
     path('ws/game/<int:game_pk>/', consumers.MasterGamePanelConsumer),
-    # path('ws/player_panel/<int:game_pk>/', consumers.PlayerPanelConsumer),
 ]
