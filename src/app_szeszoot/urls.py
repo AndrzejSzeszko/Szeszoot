4811e3e52b4ce634f9ef69f7544afd5b33a0dabd
AndrzejSzeszko <andrzejgustaw@gmail.com>
2019-01-18 00:31:33 +0100

    minor refac

--- src/app_szeszoot/urls.py
+++ src/app_szeszoot/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path('/', views.HomeView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='home'),
     path('sign_up/', views.SignInView.as_view(), name='sign-up'),
     path('login/', auth_views.LoginView.as_view(template_name='app_szeszoot/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='app_szeszoot/logout.html'), name='logout'),
